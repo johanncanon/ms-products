c5c4a609b68565c6d8163bf1875f2b8cdf4d9220
johanncanon <johanncanon@gmail.com>
2025-04-28 16:53:24 -0500

    feat: new environment injection

--- src/main/java/com/johancanon/springcloud/ms/products/services/ProductServiceImpl.java
+++ src/main/java/com/johancanon/springcloud/ms/products/services/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package com.johancanon.springcloud.ms.products.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,21 +14,32 @@ import com.johancanon.springcloud.ms.products.repositories.ProductRepository;
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
+    private final Environment environment;
 
-    public ProductServiceImpl(ProductRepository productRepository) {
+    public ProductServiceImpl(ProductRepository productRepository, Environment environment) {
         this.productRepository = productRepository;
+        this.environment = environment;
     }
 
     @Override
     @Transactional( readOnly = true )
     public List<Product> findAll() {
-        return ( List<Product> ) productRepository.findAll();
+        return (( List<Product> ) productRepository.findAll())
+                .stream()
+                .map( product -> {
+                    product.setPort( Integer.parseInt( environment.getProperty("local.server.port") ) );
+                    return product;
+                } ).toList();
     }
 
     @Override
     @Transactional( readOnly = true)
     public Optional<Product> findById(Long id) {
-        return productRepository.findById(id);
+        return productRepository.findById(id)
+        .map( product -> {
+            product.setPort( Integer.parseInt( environment.getProperty("local.server.port") ) );
+            return product; 
+        } );
     }
 
 }
